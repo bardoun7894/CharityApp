e1659e243c6c73b36e72ab8fb3eaa5b67f54da0a
drClean <bridal1wedding@gmail.com>
2020-02-26 12:01:58 +0100

    add all project

--- app/src/main/java/com/example/charityapp/ActivityCategory.java
+++ app/src/main/java/com/example/charityapp/ActivityCategory.java
@@ -2,13 +2,61 @@ package com.example.charityapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
 
 public class ActivityCategory extends AppCompatActivity {
-
+    ListView lv;
+    int[] images = {
+          R.drawable.fourni, R.drawable.clothes, R.drawable.foods, R.drawable.about,
+            R.drawable.books, R.drawable.awani, R.drawable.shoes, R.drawable.money, R.drawable.childreen_tool,
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
+        lv=findViewById(R.id.lvId);
+        CustomAdapter customAdapter =new CustomAdapter();
+        lv.setAdapter(customAdapter);
+    lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+         if(position ==0){
+             Intent intent = new Intent(ActivityCategory.this, Scro.class);
+             startActivity(intent); }
+            if(position ==3){
+                Intent intent = new Intent(ActivityCategory.this, ActivityElectric.class);
+                startActivity(intent);
+            }
+        }
+    });
+    }
+
+    class CustomAdapter extends BaseAdapter {
+        @Override
+        public int getCount() {
+            return images.length;
+        }
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+           View view =getLayoutInflater().inflate(R.layout.custom_list,null);
+            ImageView imgV=view.findViewById(R.id.imgId);
+            imgV.setImageResource(images[position]);
+            return view;
+        }
     }
 }
--- app/src/main/java/com/example/charityapp/MainActivity.java
+++ app/src/main/java/com/example/charityapp/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.charityapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+ImageView donateIV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        donateIV=findViewById(R.id.donateId);
+        donateIV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(MainActivity.this,LoginPage.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 }
